37416e834a6f34acc3e8ffdb1c5534e2fbdb1c8b
Stephi <stephiotto287@gmail.com>
2025-06-26 21:42:08 +0200

    Add basic movement to ship

--- spacewar/scripts/ship.gd
+++ spacewar/scripts/ship.gd
@@ -1,3 +1,7 @@
 extends CharacterBody2D
 
 @export var ship_data: ShipData
+
+func _process(delta: float) -> void:
+	var velocity = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
+	position += velocity * delta * 150
