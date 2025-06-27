faccbd3f41292d1ede763d40f63941c58af260df
Stephi <stephiotto287@gmail.com>
2025-06-27 17:24:48 +0200

    Add Score UI

---
+++ spacewar/scripts/UI.gd
@@ -0,0 +1,8 @@
+extends Control
+
+@onready var score_label = $Label
+
+func _process(delta: float) -> void:
+	var score_p1 = Global.score_p1
+	var score_p2 = Global.score_p2
+	score_label.text = str(score_p1,":",score_p2)
