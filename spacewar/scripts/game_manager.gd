89771c116fa01bbbe801acc0064bb6cda060028a
Stephi <stephiotto287@gmail.com>
2025-06-28 14:51:41 +0200

    Add Winning Animation

--- spacewar/scripts/game_manager.gd
+++ spacewar/scripts/game_manager.gd
@@ -1,9 +1,17 @@
 class_name GameManager
 extends Node2D
 
+@onready var win_animation = $"../AnimationPlayer"
+@onready var win_label = $"../Control/WinName"
+var win_color = [Color("eb4953"), Color("0096d1")]
+
 func _process(delta: float) -> void:
 	match Global.get_winner():
-		0: print("Spieler 1 gewinnt")
-		1: print("Spieler 2 gewinnt")
+		0: _win_animation(0)
+		1: _win_animation(1)
 		-1: pass
-		
+
+func _win_animation(id: int):
+	win_label.text = str("Player ", id+1, " wins!")
+	win_label.add_theme_color_override("font_color", win_color[id])
+	win_animation.play("WinNameBlink")
