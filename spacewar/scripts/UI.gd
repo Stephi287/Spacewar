f4e2175e2444befe4394d500d163dec27996f825
Stephi <stephiotto287@gmail.com>
2025-06-28 18:48:42 +0200

    Refactor UI script

--- spacewar/scripts/UI.gd
+++ spacewar/scripts/UI.gd
@@ -1,8 +1,20 @@
 extends Control
 
 @onready var score_label = $Label
+@onready var win_animation = $"../AnimationPlayer"
+@onready var win_label = $"WinName"
+@onready var info_label = $"RestartInfo"
+var win_color = [Color("eb4953"), Color("0096d1")]
+var win = false
 
 func _process(_delta: float) -> void:
 	var score_p1 = Global.score_p1
 	var score_p2 = Global.score_p2
 	score_label.text = str(score_p1,":",score_p2)
+
+func play_win_animation(id: int):
+	win_label.text = str("Player ", id+1, " wins!")
+	win_label.add_theme_color_override("font_color", win_color[id])
+	win_animation.play("WinNameBlink")
+	info_label.visible = true
+	win = true
--- spacewar/scripts/game_manager.gd
+++ spacewar/scripts/game_manager.gd
@@ -1,25 +1,22 @@
 class_name GameManager
 extends Node2D
 
-@onready var win_animation = $"../AnimationPlayer"
-@onready var win_label = $"../Control/WinName"
-@onready var info_label = $"../Control/RestartInfo"
-var win_color = [Color("eb4953"), Color("0096d1")]
 var win = false
+
+@onready var ui_node = $"../Control"
 
 func _process(_delta: float) -> void:
 	match Global.get_winner():
-		0: _win_animation(0)
-		1: _win_animation(1)
-		-1: pass
+		0:
+			ui_node.play_win_animation(0)
+			win = true
+		
+		1:
+			ui_node.play_win_animation(1)
+			win = true
+		-1: 
+			pass
 	if win:
 		if Input.is_action_just_pressed("START"):
 			Global.reset_global()
 			get_tree().reload_current_scene()
-
-func _win_animation(id: int):
-	win_label.text = str("Player ", id+1, " wins!")
-	win_label.add_theme_color_override("font_color", win_color[id])
-	win_animation.play("WinNameBlink")
-	info_label.visible = true
-	win = true
