60a96a36e2473f59a80ea77e5a0b48c2c9c39398
Stephi <stephiotto287@gmail.com>
2025-07-11 14:11:31 +0200

    Fix Switching of Winner

--- spacewar/scripts/game_manager.gd
+++ spacewar/scripts/game_manager.gd
@@ -1,22 +1,25 @@
 class_name GameManager
 extends Node2D
 
-var win = false
+var win_1 = false
+var win_2 = false
 
 @onready var ui_node = $"../Control"
 
 func _process(_delta: float) -> void:
 	match Global.get_winner():
 		0:
-			ui_node.play_win_animation(0)
-			win = true
+			if not win_2:
+				ui_node.play_win_animation(0)
+				win_1 = true
 		
 		1:
-			ui_node.play_win_animation(1)
-			win = true
+			if not win_1:
+				ui_node.play_win_animation(1)
+				win_2 = true
 		-1: 
 			pass
-	if win:
+	if win_1 or win_2:
 		if Input.is_action_just_pressed("START"):
 			Global.reset_global()
 			get_tree().reload_current_scene()
